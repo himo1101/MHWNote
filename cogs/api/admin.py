238a50902cc5b8ad7e0a8a77b61b3866675dfb8f
himo1101 <45819165+himo1101@users.noreply.github.com>
2020-06-06 19:32:23 +0900

    Update admin.py

--- cogs/api/admin.py
+++ cogs/api/admin.py
@@ -15,3 +15,29 @@ def func_unload(bot, module:str):
 def restart(file_name: str):
     os.system('cals')
     subprocess.run(file_name, shell=True)
+
+
+async def say_permissions(ctx, member, channel):
+        permissions = channel.permissions_for(member)
+        e = discord.Embed(colour=member.colour)
+        avatar = member.avatar_url_as(static_format='png')
+        e.set_author(name=str(member), url=avatar)
+        allowed, denied = [], []
+        for name, value in permissions:
+            name = name.replace('_', ' ').replace('guild', 'server').title()
+            if value:
+                allowed.append(name)
+            else:
+                denied.append(name)
+
+        e.add_field(name='権限有り', value='\n'.join(allowed))
+        e.add_field(name='権限無し', value='\n'.join(denied))
+        await ctx.send(embed=e)
+
+    
+async def permissions(self, ctx, member: discord.Member = None, channel: discord.TextChannel = None):
+    channel = channel or ctx.channel
+    if member is None:
+        member = ctx.author
+
+    await self.say_permissions(ctx, member, channel)
