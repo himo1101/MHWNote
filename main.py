c5605f460d5ec4e0981a20a218c8984b108332dd
himo1101 <45819165+himo1101@users.noreply.github.com>
2020-06-06 19:07:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from discord.ext import commands
 
+import botinfo
+
 class MIB(commands.Cog):
     def __init__(self):
-        super().__init__(
+        super().__init__(command_prefix = 'm:', description = botinfo.description, help_attrs=dict(hidden=True))
+
+        self.token = botinfo.token
+
+
+    async def on_ready(self):
+        print("起動しました")
