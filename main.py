1f83b33ab50ab8c5da8cc20814cde2480166d202
himo1101 <45819165+himo1101@users.noreply.github.com>
2020-06-06 19:09:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,33 @@ class MIB(commands.Cog):
         super().__init__(command_prefix = 'm:', description = botinfo.description, help_attrs=dict(hidden=True))
 
         self.token = botinfo.token
-
-
-    async def on_ready(self):
-        print("起動しました")
+        self.load_extension(f"cogs.event.start")
+ 
+ 
+    def default_embed(self, mes: str):
+        e = discord.Embed(
+            description = mes
+        )
+        return e
+        
+ 
+    async def on_message(self, mes):
+        if mes.author.bot:
+            return
+        await self.process_commands(mes)
+ 
+ 
+ 
+    async def start(self):
+        await super().start(self.token)
+ 
+    
+    def main(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.start())
+        loop.close()
+ 
+if __name__ == '__main__':
+    bot = ERP()
+    bot.main()
+ 
